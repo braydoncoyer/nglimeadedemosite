09d7ef4c285b5fda1ebc24e5e248969dec86f4fb
Braydon Coyer <braydon@coyer.net>
2019-11-20 17:56:44 -0600

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {ToastFactoryService, ToastInterface} from 'ng-limeade';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'limeade-demo-site';
+
+  constructor(private toastService: ToastFactoryService) {}
+
+  options = [
+    { value: 'success', label: 'Success' },
+    { value: 'info', label: 'Info' },
+    { value: 'warning', label: 'Warning' },
+    { value: 'error', label: 'Error' },
+  ];
+
+  option;
+
+  toastSelection = new FormGroup({
+    toastTitle: new FormControl(''),
+    toastDescription: new FormControl(''),
+    iconTitle: new FormControl('')
+  });
+
+  showToast() {
+    const toast: ToastInterface = {
+      type: this.option,
+      title: this.toastSelection.get('toastTitle').value,
+      description: this.toastSelection.get('toastDescription').value,
+      iconName: this.toastSelection.get('iconTitle').value,
+    };
+    this.toastService.showToast(toast);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {NbThemeModule, NbLayoutModule, NbInputModule, NbCardModule, NbButtonModule, NbRadioModule} from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { AppRoutingModule } from './app-routing.module';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {NgLimeadeModule} from 'ng-limeade';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({ name: 'default' }),
+    NbLayoutModule,
+    NbEvaIconsModule,
+    AppRoutingModule,
+    NbInputModule,
+    NbCardModule,
+    NbButtonModule,
+    NbRadioModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgLimeadeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
